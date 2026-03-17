87a944282b7d371b087a0136e97b32aff3960667
Ganesh2811 <gp11282001@gmail.com>
2026-03-17 18:09:56 +0530

    optimize changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
 
-import { routeNotFound, errorHandler } from "./middlewares/error.js"
+import { routeNotFound, errorHandler } from "./middleware/error.js"
 import dbconnection from "./common/dbConnection.js";
 import userRoutes from "./controller/user.js";
 import taskRoutes from "./controller/task.js";
@@ -15,20 +15,24 @@ dbconnection();
 const port = process.env.PORT || 5000;
 const app = express();
 
+// cors fix
 app.use(cors({
     origin: ["http://localhost:3000", "http://localhost:3001"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
+// routes
 app.use("/api/user", userRoutes); //  /api/user
 app.use("/api/task", taskRoutes); //  /api/task
 
+// error handling
 app.use(routeNotFound);
 app.use(errorHandler);
 
