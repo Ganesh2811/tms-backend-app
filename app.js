c452522ecef75c3e1bc0ab5140fa2e13cf26ad21
Ganesh2811 <gp11282001@gmail.com>
2026-03-17 18:04:54 +0530

    initial code format setup optimize is done

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
-import { routeNotFound, errorHandler } from "./middlewares/errorMiddleware.js"
+
+import { routeNotFound, errorHandler } from "./middlewares/error.js"
 import dbconnection from "./common/dbConnection.js";
-import routes from "./routes/index.js"
+import userRoutes from "./controller/user.js";
+import taskRoutes from "./controller/task.js";
 
 dotenv.config();
-
 dbconnection();
 
 const port = process.env.PORT || 5000;
 const app = express();
 
-
 app.use(cors({
     origin: ["http://localhost:3000", "http://localhost:3001"],
-    method: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
 
@@ -26,7 +26,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
-app.use("/api", routes);
+app.use("/api/user", userRoutes); //  /api/user
+app.use("/api/task", taskRoutes); //  /api/task
 
 app.use(routeNotFound);
 app.use(errorHandler);
